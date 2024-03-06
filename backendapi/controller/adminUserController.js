3bdfc288688de7c4dd37936cdb4fc5a66c969500
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-03-06 14:46:55 +0545

    admin user controller integration

---
+++ backendapi/controller/adminUserController.js
@@ -0,0 +1,70 @@
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ErrorHandler = require("../utils/errorHandler");
+const User = require("../models/userModel");
+const { default: mongoose } = require("mongoose");
+
+// Get all users
+const getAllUser = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+// GET SINGLE USER
+const getSingleUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with Id: ${req.params.id}`)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// UPDATE USER ROLE
+const updateUserRole = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+
+  await User.findByIdAndUpdate(req.params.id, newUserData);
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+//DELETE USER
+const deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with Id: ${req.params.id}`, 400)
+    );
+  }
+
+  await user.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully",
+  });
+});
+
+module.exports = {
+  getAllUser,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
+};
