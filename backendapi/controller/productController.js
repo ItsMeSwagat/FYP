ec175be900045a76e0c93bd2b0a9af7186e4d444
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-03-06 12:31:08 +0545

    basic product controller integration

--- backendapi/controller/productController.js
+++ backendapi/controller/productController.js
@@ -1,28 +1,27 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 // get all products
-exports.getAllProducts = async(req, res) => {
-    const products = await Product.find();
-    
-    res.status(200).json({
-        success: true,
-        products,
-    })
-}
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
 
 //get product details
-exports.getProductDetails = async(req, res) => {
-    const product = await Product.findById(req.params.id);
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-    if(!product) {
-        return res.status(500).json({
-            success: false,
-            message: "Product not found"
-        })
-    }
+  if (!product) {
+    return next(new ErrorHandler("Product Not Found", 404));
+  }
 
-    res.status(200).json({
-        success: true, 
-        product
-    })
-}
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
