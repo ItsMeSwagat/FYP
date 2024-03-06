63e9f527e9724f39524f37c41488d27fce740dcb
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-03-06 12:34:20 +0545

    authentication routing

---
+++ backendapi/routes/authRoute.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router();
+const authController = require("../controller/authController");
+
+router.post("/register", authController.registerUser);
+router.post("/login", authController.loginUser);
+router.get("/logout", authController.logoutUser);
+router.post("/password/forgot", authController.forgotPassword)
+router.put("/password/reset/:resettoken", authController.resetPassword)
+
+module.exports = router
