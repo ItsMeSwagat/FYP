2d43a43ea145f73da6515c21f751cc9af7105fa6
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-03-07 10:45:55 +0545

    review routng

---
+++ backendapi/routes/reviewRoute.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const reviewController = require("../controller/reviewController");
+const router = express.Router();
+const {
+    authenticateUser,
+  } = require("../middleware/authenticate");
+
+
+router.put("/review", authenticateUser, reviewController.createReview);
+router.get("/reviews", reviewController.getAllReviews);
+router.delete("/reviews", authenticateUser, reviewController.deleteReview);
+
+
+
+module.exports = router
