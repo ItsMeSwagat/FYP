252a33651333b4a3ba33235d43278a80089a7eaf
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-03-28 19:20:10 +0545

    loadUser integration

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../user/components/Navigation/Navbar";
 import Footer from "../user/components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import store from "../store";
+import { loadUser } from "../actions/userAction";
 
 export default function Layout() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <>
       <Navbar />
-        <Outlet />
+      <Outlet />
       <Footer />
     </>
   );
