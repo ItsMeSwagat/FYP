d8e4b24d1e2727a75f089c437b6cbdd1986d44d0
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-03-28 19:17:47 +0545

    routing updates

--- backendapi/routes/adminProductRoute.js
+++ backendapi/routes/adminProductRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const adminProductController = require("../controller/adminProductController");
+const topBannerController = require("../controller/topBannerController");
 const { authenticateUser, adminAuthenticate } = require("../middleware/authenticate");
 const router = express.Router();
 
@@ -7,5 +8,6 @@ const router = express.Router();
 router.post("/product/new", authenticateUser, adminAuthenticate("admin"), adminProductController.createProduct);
 router.put("/product/:id", authenticateUser, adminAuthenticate("admin"), adminProductController.updateProduct);
 router.delete("/product/:id", authenticateUser, adminAuthenticate("admin"), adminProductController.deleteProduct);
+router.post("/topbanner/new", authenticateUser, adminAuthenticate("admin"), topBannerController.createOrUpdateBanner);
 
 module.exports = router;
