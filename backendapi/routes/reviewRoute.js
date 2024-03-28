134d723998ac97d4eeb36186a6b08f9885652fd4
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-03-28 19:17:53 +0545

    routing updates

--- backendapi/routes/reviewRoute.js
+++ backendapi/routes/reviewRoute.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const reviewController = require("../controller/reviewController");
 const router = express.Router();
-const {
-    authenticateUser,
-  } = require("../middleware/authenticate");
-
+const { authenticateUser } = require("../middleware/authenticate");
 
 router.put("/review", authenticateUser, reviewController.createReview);
 router.get("/reviews", reviewController.getAllReviews);
 router.delete("/reviews", authenticateUser, reviewController.deleteReview);
 
-
-
-module.exports = router
+module.exports = router;
