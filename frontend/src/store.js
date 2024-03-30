a5c63e72c687693770ff330bf8b622e49854cbff
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-03-30 18:12:23 +0545

    profile reducer initialization in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,16 +2,19 @@ import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer, productDetailsReducer } from "./reducers/productReducer";
 import { userReducer } from "./reducers/userReducer";
-import {thunk}  from "redux-thunk";
+import {profileReducer} from "./reducers/profileReducer"
+import {thunk} from "redux-thunk";
 
 const rootReducer = combineReducers({
   products: productReducer,
   productDetails: productDetailsReducer,
   user: userReducer,
+  profile: profileReducer,
 });
 
 
-let initialState = {};
+let initialState = {
+};
 
 const store = configureStore({reducer: rootReducer, initialState, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)});
 
