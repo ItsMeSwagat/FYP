c12c1f3551b794c3dc5fdccb4fe38aa568c2e3c4
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-04 16:00:04 +0545

    routing updates

--- backendapi/app.js
+++ backendapi/app.js
@@ -8,19 +8,17 @@ const errorMiddleware = require("./middleware/error");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressFileUpload({
-    limits: {fileSize: 100 * 1024 * 1024} //100Mb file size
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressFileUpload());
 
 const authRouters = require("./routes/authRoute");
 app.use("/api/v1/auth", authRouters);
 
 const userRouters = require("./routes/userRoute");
-app.use("/api/v1/user", userRouters)
+app.use("/api/v1/user", userRouters);
 
 const adminUserRouters = require("./routes/adminUserRoute");
-app.use("/api/v1/admin", adminUserRouters)
+app.use("/api/v1/admin", adminUserRouters);
 
 const productRouters = require("./routes/productRoute");
 app.use("/api/v1", productRouters);
@@ -31,10 +29,15 @@ app.use("/api/v1", reviewRouters);
 const adminProductRouters = require("./routes/adminProductRoute");
 app.use("/api/v1/admin", adminProductRouters);
 
+const voucherRouters = require("./routes/voucherRoute");
+app.use("/api/v1/voucher", voucherRouters);
 
+const cartRouters = require("./routes/cartRoute");
+app.use("/api/v1/cart", cartRouters);
 
-
+const cartItemRouter = require("./routes/cartItemRoute");
+app.use("/api/v1/cart_items", cartItemRouter);
 
 app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
