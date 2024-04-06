a517533a77c0292c236fb33b585494f432c6535d
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-06 07:19:49 +0545

    profile update error fix

--- backendapi/controller/userController.js
+++ backendapi/controller/userController.js
@@ -4,6 +4,7 @@ const User = require("../models/userModel");
 const sendToken = require("../utils/userToken");
 const cloudinary = require("cloudinary");
 
+//Load User
 const getUserDetails = catchAsyncErrors(async (req, res, next) => {
   const user = await User.findById(req.user.id);
 
@@ -41,36 +42,34 @@ const updateProfile = catchAsyncErrors(async (req, res, next) => {
     email: req.body.email,
   };
 
-  const avatar = req.body.avatar;
-  console.log("Avatar", avatar);
+  const user = await User.findById(req.user.id);
 
-  if (avatar !== undefined && avatar !== '') {
-    const user = await User.findById(req.user.id);
+  if (req.body.avatar) {
+    try {
+      if (user.avatar) {
+        const imageId = user.avatar.public_id;
+        await cloudinary.v2.uploader.destroy(imageId);
+      }
 
-    const imageId = user.avatar.public_id;
-
-    await cloudinary.v2.uploader.destroy(imageId);
-
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-      folder: "avatars",
-      width: 200,
-      crop: "scale",
-    });
-
-    newUserData.avatar = {
-      public_id: myCloud.public_id,
-      url: myCloud.secure_url,
-    };
-    console.log("If working")
+      const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: "avatars",
+        width: 200,
+        crop: "scale",
+      });
+      newUserData.avatar = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
+    } catch (error) {
+      console.error("Error handling avatar:", error);
+    }
   }
 
-  
-  const user = await User.findByIdAndUpdate(req.user.id, newUserData);
-  console.log("User Updated Successfully");
+  user.set(newUserData);
+  await user.save();
 
-  res.status(200).json({
-    success: true,
-  });
+  console.log("User Updated Successfully");
+  res.status(200).json({ success: true });
 });
 
 module.exports = {
