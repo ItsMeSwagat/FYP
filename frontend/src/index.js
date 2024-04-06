e524e77ca744b9b9c12fd76aeaa81c204b68e4fe
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-06 23:45:46 +0545

    redux persist integration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./store.js";
+import { store, persistor } from "./store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.js";
-
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
