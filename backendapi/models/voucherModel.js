46a7806aa99b48470df5bf64d139db9dcfa880da
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-06 23:45:18 +0545

    voucher model

---
+++ backendapi/models/voucherModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const voucherSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    uppercase: true,
+  },
+  expiry: {
+    type: Date,
+    required: true,
+  },
+  discount: {
+    type: Number,
+    required: true,
+  },
+  discountType: {
+    type: String,
+    enum: ["percentage", "fixed"],
+    required: true,
+  },
+});
+
+const Voucher = mongoose.model("Voucher", voucherSchema);
+module.exports = Voucher;
