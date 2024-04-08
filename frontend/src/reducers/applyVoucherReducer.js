7d302fa4b945fed8ebb35185cb11591511950624
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-08 19:23:24 +0545

    voucher reducer  integration

--- frontend/src/reducers/applyVoucherReducer.js
+++ frontend/src/reducers/applyVoucherReducer.js
@@ -2,16 +2,31 @@ import {
   APPLY_VOUCHER_REQUEST,
   APPLY_VOUCHER_SUCCESS,
   APPLY_VOUCHER_FAIL,
-} from "../constants/cartConstants";
+  REMOVE_VOUCHER_REQUEST,
+  REMOVE_VOUCHER_SUCCESS,
+  REMOVE_VOUCHER_FAIL,
+  CLEAR_ERRORS,
+} from "../constants/voucherConstants";
 
-export const applyVoucherReducer = (state = {}, action) => {
+export const voucherReducer = (state = {}, action) => {
   switch (action.type) {
     case APPLY_VOUCHER_REQUEST:
-      return { loading: true };
+    case REMOVE_VOUCHER_REQUEST:
+      return { ...state, loading: true };
     case APPLY_VOUCHER_SUCCESS:
-      return { loading: false, applied: action.payload };
+      return { ...state, loading: false, applied: action.payload };
+
+    case REMOVE_VOUCHER_SUCCESS:
+      return { ...state, loading: false, removed: action.payload };
     case APPLY_VOUCHER_FAIL:
-      return { loading: false, error: action.payload };
+    case REMOVE_VOUCHER_FAIL:
+      return { ...state, loading: false, error: action.payload };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
     default:
       return state;
   }
