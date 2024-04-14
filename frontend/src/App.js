e9edf51ee49f85ba2e0ad936b94e05657e8528a8
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-15 00:54:37 +0545

    routing updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import AuthLayout from "./Layout/AuthLayout";
 import Login from "./user/components/Form/Login";
 import Signup from "./user/components/Form/Signup";
 import Layout from "./Layout/Layout";
+import AdminLayout from "./Layout/AdminLayout";
 import Home from "./user/components/Home/Home";
 import Products from "./user/components/Product/Products";
 import ProductDetails from "./user/components/Product/ProductDetails";
 import Search from "./user/components/Search/Search";
-import ProtectedRoute from "./route/ProtectedRoute";
+import { ProtectedRoute, AdminProtectedRoute } from "./route/ProtectedRoute";
 import Account from "./user/components/Account/Account";
 import EditProfile from "./user/components/Account/EditProfile";
 import ChangePassword from "./user/components/Account/ChangePassword";
@@ -23,11 +24,11 @@ import ShippingDetails from "./user/components/Checkout/ShippingDetails";
 import ConfirmOrder from "./user/components/Checkout/ConfirmOrder";
 import Payment from "./user/components/Checkout/Payment";
 import Success from "./user/components/Checkout/Success";
-import UserOrder from './user/components/Order/UserOrder'
+import Userorder from "./user/components/Order/Userorder";
+import OrderDetail from "./user/components/Order/OrderDetail";
+import AdminDashboard from "./admin/components/Dashboard/AdminDashboard";
 
 function App() {
-  const { isAuthenticated } = useSelector((state) => state.user);
-
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
@@ -35,7 +36,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AuthLayout />} errorElement={<ErrorPage />}>
+        <Route element={<AuthLayout />}>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/password/forgot" element={<ForgotPassword />} />
@@ -45,17 +46,20 @@ function App() {
           />
         </Route>
 
-        <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+        <Route element={<AdminLayout />}>
+          <Route element={<ProtectedRoute isAdmin={true} />}>
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          </Route>
+        </Route>
+
+        <Route path="/" element={<Layout />}>
           <Route path="" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/products/:keyword" element={<Products />} />
           <Route path="/search" element={<Search />} />
 
-          <Route
-            element={<ProtectedRoute isAuthenticated={isAuthenticated} />}
-            errorElement={<ErrorPage />}
-          >
+          <Route element={<ProtectedRoute />} errorElement={<ErrorPage />}>
             <Route path="/account" element={<Account />} />
             <Route path="/customer/update" element={<EditProfile />} />
             <Route path="/password/update" element={<ChangePassword />} />
@@ -64,9 +68,11 @@ function App() {
             <Route path="/confirmorder" element={<ConfirmOrder />} />
             <Route path="/payment/process" element={<Payment />} />
             <Route path="/payment/success" element={<Success />} />
-            <Route path="/orders/user" element={<UserOrder />} />
+            <Route path="/orders/user" element={<Userorder />} />
+            <Route path="/order/details/:id" element={<OrderDetail />} />
           </Route>
         </Route>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
