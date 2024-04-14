656b6f4aa9e12112e5e848239c22515f113dd222
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-15 00:56:39 +0545

    review card  integration

---
+++ frontend/src/user/components/Product/ReviewCard.js
@@ -0,0 +1,22 @@
+import { Rating } from "@mui/material";
+import React from "react";
+
+const ReviewCard = ({ review }) => {
+  const options = {
+    value: review.rating,
+    readOnly: true,
+    precision: 0.5,
+  };
+
+  return (
+    <div className=" flex-shrink-0 bg-white w-[30rem] min-h-[10rem] flex flex-col gap-[2rem] rounded-md border-2 p-4">
+      <div className=" flex flex-col gap-1">
+        <p className=" font-medium">{review.name}(User)</p>
+        <Rating {...options} />
+      </div>
+      <span className="reviewCardComment">{review.comment}</span>
+    </div>
+  );
+};
+
+export default ReviewCard;
