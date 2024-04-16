49344e63943ff0aa5a85d1a00c37d2c829a4b13b
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-16 18:00:51 +0545

    order model updates orderstatus changes

--- backendapi/models/orderModel.js
+++ backendapi/models/orderModel.js
@@ -82,7 +82,7 @@ const orderSchema = new mongoose.Schema({
   orderStatus: {
     type: String,
     required: true,
-    default: "Placed",
+    default: "Processing",
   },
   deliveredAt: Date,
   createdAt: {
