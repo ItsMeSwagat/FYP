596cfaff52b583d90b009c321bde0e0ef9f42094
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-16 18:00:04 +0545

    delete review error fix

--- backendapi/controller/reviewController.js
+++ backendapi/controller/reviewController.js
@@ -64,34 +64,39 @@ const getAllReviews = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-
 //DELETE REVIEW
 const deleteReview = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.query.productId);
 
   if (!product) {
-    return next(new ErrorHandler("Product not Found", 404));
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   const reviews = product.reviews.filter(
     (rev) => rev._id.toString() !== req.query.id.toString()
   );
 
-  let totalRating = 0;
+  let avg = 0;
 
   reviews.forEach((rev) => {
-    totalRating += rev.rating;
+    avg += rev.rating;
   });
 
-  const ratings = totalRating / reviews.length;
+  let ratings = 0;
+
+  if (reviews.length === 0) {
+    ratings = 0;
+  } else {
+    ratings = avg / reviews.length;
+  }
 
   const numOfReviews = reviews.length;
 
   await Product.findByIdAndUpdate(req.query.productId, {
     reviews,
     ratings,
-    numOfReviews
-  })
+    numOfReviews,
+  });
 
   res.status(200).json({
     success: true,
@@ -101,5 +106,5 @@ const deleteReview = catchAsyncErrors(async (req, res, next) => {
 module.exports = {
   createReview,
   getAllReviews,
-  deleteReview
+  deleteReview,
 };
