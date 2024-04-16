acfc55403ba0d247bac7c1d0de67dd2f6eee9cba
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-16 18:01:20 +0545

    admin product route updates

--- backendapi/routes/adminProductRoute.js
+++ backendapi/routes/adminProductRoute.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const adminProductController = require("../controller/adminProductController");
 const topBannerController = require("../controller/topBannerController");
-const { authenticateUser, adminAuthenticate } = require("../middleware/authenticate");
+const {
+  authenticateUser,
+  adminAuthenticate,
+} = require("../middleware/authenticate");
 const router = express.Router();
 
-
-router.post("/product/new", authenticateUser, adminAuthenticate("admin"), adminProductController.createProduct);
-router.put("/product/:id", authenticateUser, adminAuthenticate("admin"), adminProductController.updateProduct);
-router.delete("/product/:id", authenticateUser, adminAuthenticate("admin"), adminProductController.deleteProduct);
-router.post("/topbanner/new", authenticateUser, adminAuthenticate("admin"), topBannerController.createOrUpdateBanner);
+router.get(
+  "/products",
+  authenticateUser,
+  adminAuthenticate("admin"),
+  adminProductController.getAllProductsAdmin
+);
+router.post(
+  "/product/create",
+  authenticateUser,
+  adminAuthenticate("admin"),
+  adminProductController.createProduct
+);
+router.put(
+  "/product/:id",
+  authenticateUser,
+  adminAuthenticate("admin"),
+  adminProductController.updateProduct
+);
+router.delete(
+  "/product/:id",
+  authenticateUser,
+  adminAuthenticate("admin"),
+  adminProductController.deleteProduct
+);
+router.post(
+  "/topbanner/new",
+  authenticateUser,
+  adminAuthenticate("admin"),
+  topBannerController.createOrUpdateBanner
+);
 
 module.exports = router;
