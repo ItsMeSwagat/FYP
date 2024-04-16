152f4440ee7e2198572b283b8217876c8430f406
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-04-16 18:09:43 +0545

    reducer updates

--- frontend/src/reducers/orderReducer.js
+++ frontend/src/reducers/orderReducer.js
@@ -9,6 +9,17 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ALL_ORDERS_REQUEST,
+  ALL_ORDERS_SUCCESS,
+  ALL_ORDERS_FAIL,
+  ADMIN_UPDATE_ORDER_REQUEST,
+  ADMIN_DELETE_ORDER_REQUEST,
+  ADMIN_UPDATE_ORDER_SUCCESS,
+  ADMIN_DELETE_ORDER_SUCCESS,
+  ADMIN_UPDATE_ORDER_FAIL,
+  ADMIN_DELETE_ORDER_FAIL,
+  ADMIN_UPDATE_ORDER_RESET,
+  ADMIN_DELETE_ORDER_RESET,
 } from "../constants/orderConstants";
 
 export const createOrderReducer = (state = {}, action) => {
@@ -87,6 +98,87 @@ export const orderDetailsReducer = (state = { order: {} }, action) => {
       return {
         loading: false,
         error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const getAllOrdersReducer = (state = { orders: [] }, action) => {
+  switch (action.type) {
+    case ALL_ORDERS_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case ALL_ORDERS_SUCCESS:
+      return {
+        loading: false,
+        orders: action.payload,
+      };
+
+    case ALL_ORDERS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const orderReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ADMIN_UPDATE_ORDER_REQUEST:
+    case ADMIN_DELETE_ORDER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case ADMIN_UPDATE_ORDER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isUpdated: action.payload,
+      };
+
+    case ADMIN_DELETE_ORDER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isDeleted: action.payload,
+      };
+
+    case ADMIN_UPDATE_ORDER_FAIL:
+    case ADMIN_DELETE_ORDER_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case ADMIN_UPDATE_ORDER_RESET:
+      return {
+        ...state,
+        isUpdated: false,
+      };
+
+    case ADMIN_DELETE_ORDER_RESET:
+      return {
+        ...state,
+        isDeleted: false,
       };
     case CLEAR_ERRORS:
       return {
--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -6,15 +6,30 @@ import {
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
   CLEAR_ERRORS,
-
+  ADMIN_PRODUCT_REQUEST,
+  ADMIN_PRODUCT_SUCCESS,
+  ADMIN_PRODUCT_FAIL,
+  CREATE_PRODUCT_REQUEST,
+  CREATE_PRODUCT_SUCCESS,
+  CREATE_PRODUCT_FAIL,
+  CREATE_PRODUCT_RESET,
+  ADMIN_DELETE_PRODUCT_REQUEST,
+  ADMIN_UPDATE_PRODUCT_REQUEST,
+  ADMIN_DELETE_PRODUCT_SUCCESS,
+  ADMIN_UPDATE_PRODUCT_SUCCESS,
+  ADMIN_DELETE_PRODUCT_FAIL,
+  ADMIN_UPDATE_PRODUCT_FAIL,
+  ADMIN_DELETE_PRODUCT_RESET,
+  ADMIN_UPDATE_PRODUCT_RESET,
 } from "../constants/productConstants";
 
-export const productReducer = (state = { products: [] }, action) => {
+export const productsReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case ALL_PRODUCT_REQUEST:
+    case ADMIN_PRODUCT_REQUEST:
       return {
         loading: true,
-        product: [],
+        products: [],
       };
 
     case ALL_PRODUCT_SUCCESS:
@@ -26,7 +41,14 @@ export const productReducer = (state = { products: [] }, action) => {
         filteredProductsCount: action.payload.filteredProductsCount,
       };
 
+    case ADMIN_PRODUCT_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload,
+      };
+
     case ALL_PRODUCT_FAIL:
+    case ADMIN_PRODUCT_FAIL:
       return {
         loading: false,
         error: action.payload,
@@ -61,6 +83,88 @@ export const productDetailsReducer = (state = { product: {} }, action) => {
         error: action.payload,
       };
 
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const createProductReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case CREATE_PRODUCT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case CREATE_PRODUCT_SUCCESS:
+      return {
+        loading: false,
+        success: action.payload.success,
+        product: action.payload.product,
+      };
+    case CREATE_PRODUCT_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case CREATE_PRODUCT_RESET:
+      return {
+        ...state,
+        success: false,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const productReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ADMIN_DELETE_PRODUCT_REQUEST:
+    case ADMIN_UPDATE_PRODUCT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ADMIN_DELETE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isDeleted: action.payload,
+      };
+
+    case ADMIN_UPDATE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isUpdated: action.payload,
+      };
+    case ADMIN_DELETE_PRODUCT_FAIL:
+    case ADMIN_UPDATE_PRODUCT_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case ADMIN_DELETE_PRODUCT_RESET:
+      return {
+        ...state,
+        isDeleted: false,
+      };
+    case ADMIN_UPDATE_PRODUCT_RESET:
+      return {
+        ...state,
+        isUpdated: false,
+      };
     case CLEAR_ERRORS:
       return {
         ...state,
