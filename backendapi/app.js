c9b123fcc25e82bdda87d0fc434ae125a42f3a5c
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-06-04 20:46:11 +0545

    cors bug fix

--- backendapi/app.js
+++ backendapi/app.js
@@ -16,7 +16,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "https://jass-flame.vercel.app",
+    origin: "*",
     methods: "GET,PUT,POST,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
